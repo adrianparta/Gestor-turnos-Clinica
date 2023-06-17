57cfb1276256a26750d7195fa5774a29d8d84372
Agustin Sbernini <fliasbernini@hotmail.com>
2023-06-17 17:07:05 -0300

    redirect a Login

--- Clinic/MasterPage.Master.cs
+++ Clinic/MasterPage.Master.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
 
 namespace Clinic
 {
@@ -11,7 +13,15 @@ namespace Clinic
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!EstaLogeado() && !(Page is Login))
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
 
+        private bool EstaLogeado()
+        {
+            return !(Session["Usuario"] is null);
         }
     }
 }
